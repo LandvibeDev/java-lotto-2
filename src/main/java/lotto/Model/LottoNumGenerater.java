8c318fce551bc9318ca89296e332af3cd8457f00
wldnd7145 <80200734+wldnd7145@users.noreply.github.com>
2024-07-18 19:55:30 +0900

    Add files via upload

---
+++ src/main/java/lotto/Model/LottoNumGenerater.java
@@ -0,0 +1,21 @@
+package lotto.Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumGenerater {
+
+    public List<Lotto> generateLottos(int count) {
+        List<Lotto> lottoNumList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            lottoNumList.add(new Lotto(numbers));
+            System.out.println(numbers);
+        }
+        return lottoNumList;
+    }
+}
